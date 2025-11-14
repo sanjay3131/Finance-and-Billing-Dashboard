b34e6179fa98e7da7a5af45bf21eb911b1db92b3
sanjay3131 <sanjay31ramuk@gmail.com>
2025-11-14 16:27:17 +0530

    page limit and sort function are added in expense route

--- Backend/controllers/expenseController.js
+++ Backend/controllers/expenseController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import Expense from "../models/expense.js";
+import Shop from "../models/shop.js";
 // create expense
 export const addExpense = asyncHandler(async (req, res) => {
   const shopId = req.shop._id;
@@ -66,18 +67,30 @@ export const getAllExpenses = asyncHandler(async (req, res) => {
 
   const { from = startOfToday, to = endOfToday } = req.body;
   console.log(from, to);
-
-  const expenses = await Expense.find({
+  // sorting
+  const sortBy = req.query.sort || "-expenseDate";
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  const filter = {
     Shop: shopId,
     expenseDate: { $gte: from, $lte: to },
-  }).sort({
-    expenseDate: -1,
-  });
+  };
 
+  const expenses = await Expense.find(filter)
+    .sort(sortBy)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Expense.countDocuments(filter);
   res.status(200).json({
     success: true,
+    total, // total docs
+    page,
+    limit,
+    pages: Math.ceil(total / limit),
     data: expenses,
-    count: expenses.length,
     message: `${expenses.length} Expenses fetched successfully`,
   });
 });

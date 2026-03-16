6891791537fd54fe6b57184822a8993ba7d372c5
sanjay3131 <sanjay31ramuk@gmail.com>
2026-03-16 15:37:19 +0530

    cors sameSite is changed to none

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (shopId, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
   console.log(" Generated Token:");
